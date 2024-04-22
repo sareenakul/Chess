92661614ed2d2c251af10ae17c2a33c6110c7a72
Akul Sareen <sareen.akul@gmail.com>
2024-04-22 15:58:38 -0400

    Implementation of chess.js

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,22 +1,57 @@
+import { Chess } from "chess.js";
 import { WebSocket } from "ws";
+import { GAME_OVER } from "./message";
 
 export class Game{
     public player1: WebSocket;
     public player2: WebSocket;
-    private board: string;
-    private moves: string[];
+    public board: Chess;
     private startTime: Date;
 
 
     constructor(player1: WebSocket, player2: WebSocket){
         this.player1 = player1;
         this.player2 = player2;
-        this.board = "";
-        this.moves = [];
+        this.board = new Chess();
         this.startTime = new Date();
     }
 
-    makeMove(socket: WebSocket, move: string) {
+    makeMove(socket: WebSocket, moveToMake: { from : string; to: string;}) {
+        //Doesn't let player1 play when it's not it's turn.
+        if(this.board.moves.length % 2 === 1 && socket != this.player1){
+            return;
+        }
+
+        //Doesn't let player1 play when it's not it's turn.
+        if(this.board.moves.length % 2 !== 1 && socket != this.player2){
+            return;
+        }
+
+        try{
+            this.board.move(moveToMake);
+        }
+        catch(e){
+            return;
+        }
+
+        //Once Game is over, display the result to both the users
+        if(this.board.isGameOver()){
+            this.player1.emit(JSON.stringify({
+                type: GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === 'w'? "black" : "white"
+                }
+            }))
+
+            this.player2.emit(JSON.stringify({
+                type: GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === 'w'? "black" : "white"
+                }
+            }))
+            return;
+        }
+
         //Validate the right user's move
         //Validate the move
         //Update the board
