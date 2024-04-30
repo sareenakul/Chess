0f1ddf985c0f57a08f87ec6997b3ec14430e7aab
Akul Sareen <sareen.akul@gmail.com>
2024-04-30 14:13:46 -0400

    Starting with the game and board logic

--- chessfrontend/src/Pages/Game.tsx
+++ chessfrontend/src/Pages/Game.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import { Chessboard } from "../Components/Chessboard";
 import { PlayButton } from "../Components/PlayButton";
 import { useSocket } from "../Hooks/useSocket";
+import { Chess } from "chess.js";
 
 export const INIT_GAME = "init_game";
 export const MOVE = "move";
@@ -8,6 +10,31 @@ export const GAME_OVER = "game_over";
 
 export const Game = () =>{
     const socket = useSocket();
+    const [board, setBoard] = useState(new Chess());
+
+    useEffect(() =>{
+        if(!socket){
+            return;
+        }
+        socket.onmessage = (event) =>{
+            const message = JSON.parse(event.data);
+            console.log(message);
+            switch(message.type){
+                case INIT_GAME:
+                    setBoard(new Chess());
+                    console.log("Game Initialized");
+                    break;
+                case MOVE:
+                    const move = message.payload;
+                    board.move(move);
+                    console.log("Played Move");
+                    break;
+                case GAME_OVER:
+                    console.log("Game Over");
+                    break;
+            }
+        }
+    },[socket])
     if(!socket){
         return(
             <div>Connecting...</div>
@@ -24,7 +51,7 @@ export const Game = () =>{
                         <PlayButton onClick={()=> {
                             socket.send(JSON.stringify({
                                 type: INIT_GAME,
-                                
+
                             }))}}>
                                 <h1>Play</h1>
                         </PlayButton>
